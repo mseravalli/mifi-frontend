91f836589bcc0c3fe1521ff5f8221929fa15d592
Marco Seravalli <marco.seravalli@accenture.com>
2017-10-12 16:04:01 +0200

    store functions in separate file

---
+++ src/app/utils.ts
@@ -0,0 +1,28 @@
+export class Utils {
+  static formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
+
+  static handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  static assignColors(categories:any, colorTable:any) {
+    var colors = [];
+    for (var i = 0; i < categories.length; ++i) {
+      if (categories[i] !== "date") {
+        colors.push(colorTable[categories[i]]);
+      }
+    }
+    return colors;
+  }
+}
