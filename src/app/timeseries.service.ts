6ffd3bd397725e672e76b00f2c5e3471ef9c2590
Marco Seravalli <marco.seravalli@accenture.com>
2017-10-12 16:04:40 +0200

    use functions in utils

--- src/app/timeseries.service.ts
+++ src/app/timeseries.service.ts
@@ -1,39 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import { Category } from './category';
+import { Utils } from './utils';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class TimeseriesService {
-  private timeseriesUrl = 'http://localhost:9000/api/v0.1/accounts/timeseries';
+  private url = 'http://localhost:9000/api/v0.1/accounts/timeseries';
 
   constructor(private http: Http) { }
 
-  formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, month, day].join('-');
-  }
-
   getTimeseries(range: String, startDate: Date, endDate: Date): Promise<Array<any>> {
     var parameters: String = "?sumRange=" + range
-      + "&startDate=" + this.formatDate(startDate)
-      + "&endDate=" + this.formatDate(endDate);
-    return this.http.get(this.timeseriesUrl + parameters)
+      + "&startDate=" + Utils.formatDate(startDate)
+      + "&endDate=" + Utils.formatDate(endDate);
+    return this.http.get(this.url + parameters)
       .toPromise()
       .then(response => response.json().data)
-      .catch(this.handleError);
+      .catch(Utils.handleError);
   }
-
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
-
 }
