a351a0f031203db4c09f6b3cdbb47efe4cfabe91
Marco Seravalli <seravalli@google.com>
2018-06-13 12:50:59 +0200

    use version from package.json for docker image

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+VERSION=$(jq -r '.version' package.json)
 ng build --prod
-docker build -t mseravalli/mifi-frontend:$(cat VERSION) .
+docker build -t mseravalli/mifi-frontend:${VERSION} .
+docker push mseravalli/mifi-frontend:${VERSION}
