c05f1ccf2dc64ffbe69ef295fd06b5bd7cda4369
Marco Seravalli <seravalli@google.com>
2018-06-14 13:47:34 +0200

    add chart for account distribution

--- src/app/account.ts
+++ src/app/account.ts
@@ -2,11 +2,13 @@ export class Account {
   name:          string;
   color:         string;
   selected:      boolean;
+  balance:       number;
 
   constructor(name: string, color: string, selected: boolean) {
     this.name     = name;
     this.color    = color;
     this.selected = selected;
+    this.balance  = 1.0;
   }
 }
 
